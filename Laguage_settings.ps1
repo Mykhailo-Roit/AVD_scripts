2a869719d1638d2a57312df42f483f9f59503e08
Mykhailo Roit <88772563+Mykhailo-Roit@users.noreply.github.com>
2023-12-31 15:41:34 +0200

    Update Laguage_settings.ps1
    fix parameters type

--- Laguage_settings.ps1
+++ Laguage_settings.ps1
@@ -1,9 +1,9 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param (
     [Parameter(
         Mandatory
     )]
-    [System.Int16[]]$lang_WinHomeLocation,
+    [System.Int32]$lang_WinHomeLocation,
     [Parameter(
         Mandatory
     )]
@@ -17,10 +17,10 @@ Write-Host "Set-WinHomeLocation  -GeoId  $lang_WinHomeLocation"
 Set-WinHomeLocation  -GeoId  $lang_WinHomeLocation
 
 Write-Host "Set-WinSystemLocale -SystemLocale $lang_code"
-Set-WinSystemLocale -SystemLocale $lang_code
+Set-WinSystemLocale -SystemLocale "$lang_code"
 
 Write-Host "Set-Culture $lang_code"
-Set-Culture $lang_code
+Set-Culture "$lang_code"
 
 Write-Host "Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True"
 Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
