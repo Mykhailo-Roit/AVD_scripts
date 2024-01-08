700e15a2518242934eb2fa8d3ab9216343ad2c6e
Mykhailo Roit <88772563+Mykhailo-Roit@users.noreply.github.com>
2024-01-08 14:42:18 +0200

    Update Office_install.ps1

--- Office_install.ps1
+++ Office_install.ps1
@@ -2,10 +2,10 @@ $tmpFolder = "c:\temp"
 mkdir $tmpFolder
 cd $tmpFolder
 Invoke-WebRequest -Uri https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16731-20398.exe -OutFile officedeploymenttool_16731-20398.exe
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mykhailo-Roit/AVD_scripts/main/office-Configuration-all-languages.xml -Outfile office-Configuration-all-languages.xml
 .\officedeploymenttool_16731-20398.exe /extract:./office /quiet 
 cd office
 
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mykhailo-Roit/AVD_scripts/main/office-Configuration-all-languages.xml -Outfile office-Configuration-all-languages.xml
 .\setup.exe /download ..\office-Configuration-all-languages.xml  
 .\setup.exe /configure ..\office-Configuration-all-languages.xml
 cd ..
