d713158e46fce197a819e99e22891f179c13a14b
Mykhailo Roit <88772563+Mykhailo-Roit@users.noreply.github.com>
2024-01-09 10:51:39 +0200

    Update Office_install.ps1

--- Office_install.ps1
+++ Office_install.ps1
@@ -1,22 +1,28 @@
-$tmpFolder = "c:\temp"
-mkdir $tmpFolder
+Set-PSDebug -Trace 1
+
+$tmpFolder = "c:\temp\psscript"
+mkdir $tmpFolder -Force
 cd $tmpFolder
 
 # https://www.microsoft.com/en-us/download/details.aspx?id=49117
-Invoke-WebRequest -Uri https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16731-20398.exe -OutFile officedeploymenttool_16731-20398.exe
+Invoke-WebRequest -Uri https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16731-20398.exe -OutFile officedeploymenttool_16731-20398.exe -verbose
+dir
 
 # https://learn.microsoft.com/en-us/deployoffice/overview-office-deployment-tool
 # https://config.office.com/deploymentsettings
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mykhailo-Roit/AVD_scripts/main/office-Configuration-all-languages.xml -Outfile office-Configuration-all-languages.xml
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/Mykhailo-Roit/AVD_scripts/main/office-Configuration-all-languages.xml -Outfile office-Configuration-all-languages.xml -verbose
+dir
 .\officedeploymenttool_16731-20398.exe /extract:./office /quiet 
+sleep 20
 cd office
 
 .\setup.exe /download ..\office-Configuration-all-languages.xml  
 .\setup.exe /configure ..\office-Configuration-all-languages.xml
-cd ..
+cd \
 #Get-ChildItem -Path ./office | Remove-Item -Force -Recurse
 #rmdir ./office
 
-Get-ChildItem -Path $tmpFolder | Remove-Item -Force -Recurse
-cd c:\
+
+Get-ChildItem -Path $tmpFolder -Recurse | Remove-Item -Force -Recurse
+#cd c:\
 rmdir $tmpFolder
